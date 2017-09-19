0be9651339c8b8f656339147f810af3ae25f3384
Kocso Sandor Gabor <kocso.sandor.gabor@gmail.com>
2017-09-19 08:05:42 +0200

    work scheduler

--- src/test/java/hu/kits/timesheet/schedule/WorkScheduler.java
+++ src/test/java/hu/kits/timesheet/schedule/WorkScheduler.java
@@ -14,6 +14,7 @@ public class WorkScheduler {
 		int n = 100_000_000;
 		for(int i=0;i<n;i++) {
 			WeeklySchedule schedule = createSchedule();
+			//System.out.println(schedule);
 			if(testSchedule(schedule)) {
 				System.out.println(schedule);
 				return;
@@ -34,19 +35,24 @@ public class WorkScheduler {
 		
 		List<DailySchedule> dailySchedules = new ArrayList<>();
 		for(int i=1;i<=5;i++) {
-			WorkerSchedule worker1Schedule = randomWorkerSchedule(worker1);
-			WorkerSchedule worker2Schedule = randomWorkerSchedule(worker2);
-			WorkerSchedule worker3Schedule = randomWorkerSchedule(worker3);
+			WorkerSchedule worker1Schedule = randomWorkerSchedule(worker1, i);
+			WorkerSchedule worker2Schedule = randomWorkerSchedule(worker2, i);
+			WorkerSchedule worker3Schedule = randomWorkerSchedule(worker3, i);
 			dailySchedules.add(new DailySchedule(Arrays.asList(worker1Schedule, worker2Schedule, worker3Schedule)));
 		}
 		
 		return new WeeklySchedule(dailySchedules);
 	}
 	
-	private static WorkerSchedule randomWorkerSchedule(Worker worker) {
-		int from = random.nextInt(2) + 10;
+	private static WorkerSchedule randomWorkerSchedule(Worker worker, int day) {
+		if(day == 5 && (40 - worker.hoursWorked) <= 9) {
+			int hours = 40 - worker.hoursWorked;
+			//int from = random.nextInt(4) + 10;
+			return new WorkerSchedule(worker, 10, 10 + hours - 1);
+		}
+		int from = random.nextInt(4) + 10;
 		int maxHours = 19 - from;
-		int hours = generateRandomHours(8, maxHours);
+		int hours = generateRandomHours(4, maxHours);
 		worker.work(hours);
 		return new WorkerSchedule(worker, from, from + hours - 1);
 	}
